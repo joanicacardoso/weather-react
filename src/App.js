69d9cc46ddf582820d8c8c188bedb11b77dfc0ac
joanicacardoso <joana.cardoso.reis@gmail.com>
2020-08-30 11:39:23 +0100

    Added loader and changed to white

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello!!!!!</h1>
+        <Loader type="Puff" color="#ffffff" height={100} width={100} />
       </header>
     </div>
   );
